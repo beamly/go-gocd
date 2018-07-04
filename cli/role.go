956c2613bce89164a6111dac445561b1165c7c97
Drew J. Sonne <drew.sonne@beamly.com>
2018-07-04 11:12:14 +0100

    Added cli implementation for the role functionality

--- cli/role.go
+++ cli/role.go
@@ -2,18 +2,18 @@ package cli
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"github.com/beamly/go-gocd/gocd"
 	"github.com/urfave/cli"
-	"errors"
 	"io/ioutil"
-	"encoding/json"
 )
 
 // List of command name and descriptions
 const (
 	CreateRoleCommandName  = "create-role"
 	CreateRoleCommandUsage = "Create a role"
-	ListRoleCommandName    = "list-role"
+	ListRoleCommandName    = "list-roles"
 	ListRoleCommandUsage   = "List all the roles"
 	GetRoleCommandName     = "get-role"
 	GetRoleCommandUsage    = "Get a Role"
@@ -29,13 +29,13 @@ func createRoleAction(client *gocd.Client, c *cli.Context) (r interface{}, resp 
 		return nil, nil, NewFlagError("name")
 	}
 
-	roleJson := c.String("role-json")
+	roleJSON := c.String("role-json")
 	roleFile := c.String("role-file")
-	if roleJson == "" && roleFile == "" {
+	if roleJSON == "" && roleFile == "" {
 		return nil, nil, errors.New("One of '--role-file' or '--role-json' must be specified")
 	}
 
-	if roleJson != "" && roleFile != "" {
+	if roleJSON != "" && roleFile != "" {
 		return nil, nil, errors.New("Only one of '--role-file' or '--role-json' can be specified")
 	}
 
@@ -46,7 +46,7 @@ func createRoleAction(client *gocd.Client, c *cli.Context) (r interface{}, resp 
 			return nil, nil, err
 		}
 	} else {
-		rf = []byte(roleJson)
+		rf = []byte(roleJSON)
 	}
 	role := &gocd.Role{}
 	err = json.Unmarshal(rf, &role)
@@ -66,14 +66,73 @@ func getRoleAction(client *gocd.Client, c *cli.Context) (r interface{}, resp *go
 		return nil, nil, NewFlagError("name")
 	}
 
-	return client.Role.Get(context.Background(), c.String("name"))
+	getResponse, resp, err := client.Roles.Get(context.Background(), name)
+	if resp.HTTP.StatusCode != 404 {
+		getResponse.RemoveLinks()
+	}
+	return getResponse, resp, err
 }
 
 // ListRoleAction retrieves all role configurations
 func listRoleAction(client *gocd.Client, c *cli.Context) (r interface{}, resp *gocd.APIResponse, err error) {
-	return client.Role.List(context.Background())
+	return client.Roles.List(context.Background())
 }
 
+// UpdateRoleAction handles the interaction between the cli flags and the action handler for
+// update-role-action
+func updateRoleAction(client *gocd.Client, c *cli.Context) (r interface{}, resp *gocd.APIResponse, err error) {
+	var name, version string
+
+	if name = c.String("name"); name == "" {
+		return nil, nil, NewFlagError("name")
+	}
+
+	if version = c.String("role-version"); version == "" {
+		return nil, nil, NewFlagError("role-version")
+	}
+
+	role := c.String("role")
+	roleFile := c.String("role-file")
+	if role == "" && roleFile == "" {
+		return nil, nil, errors.New("One of '--role-file' or '--role' must be specified")
+	}
+
+	if role != "" && roleFile != "" {
+		return nil, nil, errors.New("Only one of '--role-file' or '--role' can be specified")
+	}
+
+	var pf []byte
+	if roleFile != "" {
+		pf, err = ioutil.ReadFile(roleFile)
+		if err != nil {
+			return nil, nil, err
+		}
+	} else {
+		pf = []byte(role)
+	}
+	p := &gocd.Role{
+		Version: version,
+	}
+	err = json.Unmarshal(pf, &p)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return client.Roles.Update(context.Background(), name, p)
+}
+
+func deleteRoleAction(client *gocd.Client, c *cli.Context) (r interface{}, resp *gocd.APIResponse, err error) {
+	name := c.String("name")
+	if name == "" {
+		return nil, nil, NewFlagError("name")
+	}
+
+	deleteResponse, resp, err := client.Roles.Delete(context.Background(), name)
+	if resp.HTTP.StatusCode == 406 {
+		err = errors.New(deleteResponse)
+	}
+	return deleteResponse, resp, err
+}
 func createRoleCommand() *cli.Command {
 	return &cli.Command{
 		Name:     CreateRoleCommandName,
@@ -92,7 +151,7 @@ func listRoleCommand() *cli.Command {
 	return &cli.Command{
 		Name:     ListRoleCommandName,
 		Usage:    ListRoleCommandUsage,
-		Category: "Role",
+		Category: "Roles",
 		Action:   ActionWrapper(listRoleAction),
 	}
 }
@@ -101,7 +160,7 @@ func getRoleCommand() *cli.Command {
 	return &cli.Command{
 		Name:     GetRoleCommandName,
 		Usage:    GetRoleCommandUsage,
-		Category: "Role",
+		Category: "Roles",
 		Action:   ActionWrapper(getRoleAction),
 		Flags: []cli.Flag{
 			cli.StringFlag{Name: "name"},
@@ -113,7 +172,7 @@ func deleteRoleCommand() *cli.Command {
 	return &cli.Command{
 		Name:     DeleteRoleCommandName,
 		Usage:    DeleteRoleCommandUsage,
-		Category: "Role",
+		Category: "Roles",
 		Action:   ActionWrapper(deleteRoleAction),
 	}
 }
@@ -122,7 +181,7 @@ func updateRoleCommand() *cli.Command {
 	return &cli.Command{
 		Name:     UpdateRoleCommandName,
 		Usage:    UpdateRoleCommandUsage,
-		Category: "Role",
+		Category: "Roles",
 		Action:   ActionWrapper(updateRoleAction),
 	}
 }
