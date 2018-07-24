978dd113a668fc581f3295f81e8f5f804889b2db
Drew J. Sonne <drew.sonne@gmail.com>
2018-07-24 21:02:39 +0100

    Use docker integration test

--- gocd/server_version_test.go
+++ gocd/server_version_test.go
@@ -2,10 +2,7 @@ package gocd
 
 import (
 	"context"
-	"fmt"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"net/http"
 	"testing"
 )
 
@@ -17,39 +14,30 @@ func TestServerVersion(t *testing.T) {
 }
 
 func testServerVersion(t *testing.T) {
-	setup()
-	defer teardown()
 
-	mux.HandleFunc("/api/version", func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, r.Method, "GET", "Unexpected HTTP method")
-		assert.Equal(t, apiV1, r.Header.Get("Accept"))
+	if runIntegrationTest() {
 
-		j, _ := ioutil.ReadFile("test/resources/server-version.v1.1.json")
+		cachedServerVersion = nil
+		v, _, err := client.ServerVersion.Get(context.Background())
 
-		fmt.Fprint(w, string(j))
-	})
+		assert.NoError(t, err)
 
-	cachedServerVersion = nil
-	v, _, err := client.ServerVersion.Get(context.Background())
+		assert.Equal(t, &ServerVersion{
+			Version:     "16.6.0",
+			BuildNumber: "3348",
+			GitSha:      "a7a5717cbd60c30006314fb8dd529796c93adaf0",
+			FullVersion: "16.6.0 (3348-a7a5717cbd60c30006314fb8dd529796c93adaf0)",
+			CommitURL:   "https://github.com/gocd/gocd/commits/a7a5717cbd60c30006314fb8dd529796c93adaf0",
+			VersionParts: &ServerVersionParts{
+				Major: 16,
+				Minor: 6,
+				Patch: 0,
+			},
+		}, v)
 
-	assert.NoError(t, err)
-
-	assert.Equal(t, &ServerVersion{
-		Version:     "16.6.0",
-		BuildNumber: "3348",
-		GitSha:      "a7a5717cbd60c30006314fb8dd529796c93adaf0",
-		FullVersion: "16.6.0 (3348-a7a5717cbd60c30006314fb8dd529796c93adaf0)",
-		CommitURL:   "https://github.com/gocd/gocd/commits/a7a5717cbd60c30006314fb8dd529796c93adaf0",
-		VersionParts: &ServerVersionParts{
-			Major: 16,
-			Minor: 6,
-			Patch: 0,
-		},
-	}, v)
-
-	// Verify that the server version is cached
-	assert.Equal(t, cachedServerVersion, v)
-
+		// Verify that the server version is cached
+		assert.Equal(t, cachedServerVersion, v)
+	}
 }
 
 func testBadServerVersion(t *testing.T) {
@@ -68,29 +56,13 @@ func testBadServerVersion(t *testing.T) {
 }
 
 func testBadServerVersionMajor(t *testing.T, i int, errString string) {
-	setup()
-	defer teardown()
-
-	mux.HandleFunc("/api/version", func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, r.Method, "GET", "Unexpected HTTP method")
-		assert.Equal(t, apiV1, r.Header.Get("Accept"))
-
-		j, _ := ioutil.ReadFile(
-			fmt.Sprintf("test/resources/server-version.v1.%d.json", i),
-		)
-
-		fmt.Fprint(w, string(j))
-	})
-
-	_, _, err := client.ServerVersion.Get(context.Background())
-
-	assert.EqualError(t, err, errString)
+	if runIntegrationTest() {
+		_, _, err := client.ServerVersion.Get(context.Background())
+		assert.EqualError(t, err, errString)
+	}
 }
 
 func testServerVersionCaching(t *testing.T) {
-	setup()
-	defer teardown()
-	// Note that this test should not do an API call
 
 	cachedServerVersion = &ServerVersion{
 		Version:     "18.7.0",
