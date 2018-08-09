93276c1765ebef7fcdd8f915ad4b3f02696a5130
Drew J. Sonne <drew.sonne@gmail.com>
2018-08-09 10:15:21 +0100

    Use integration tests instead of mocks

--- gocd/pipeline_action_test.go
+++ gocd/pipeline_action_test.go
@@ -10,41 +10,48 @@ import (
 
 func testPipelineServicePause(t *testing.T) {
 	for n, test := range []struct {
+		name          string
 		v             *ServerVersion
 		confirmHeader string
 		acceptHeader  string
 	}{
 		{
+			name:          "server-version-14.3.0",
 			v:             &ServerVersion{Version: "14.3.0"},
 			confirmHeader: "Confirm",
 			acceptHeader:  apiV0,
 		},
 		{
+			name:          "server-version-18.3.0",
 			v:             &ServerVersion{Version: "18.3.0"},
 			confirmHeader: "X-GoCD-Confirm",
 			acceptHeader:  apiV1,
 		},
 	} {
-		err := test.v.parseVersion()
-		assert.NoError(t, err)
+		t.Run(test.name, func(t *testing.T) {
+			if runIntegrationTest(t) {
 
-		cachedServerVersion = test.v
-		// defaultHTTPMux doesn't support multiple registrations so change the url a bit
-		mux.HandleFunc(fmt.Sprintf("/api/pipelines/test-pipeline%d/pause", n), func(w http.ResponseWriter, r *http.Request) {
-			assert.Equal(t, r.Method, "POST", "Unexpected HTTP method")
-			assert.Equal(t, "true", r.Header.Get(test.confirmHeader))
-			if test.acceptHeader == "" {
-				assert.Equal(t, len(r.Header["Accept"]), 0)
-			} else {
-				assert.Contains(t, r.Header["Accept"], test.acceptHeader)
+				pipelineName := fmt.Sprintf("test-pipeline-un-pause%d", n)
+
+				err := test.v.parseVersion()
+				assert.NoError(t, err)
+
+				cachedServerVersion = test.v
+
+				ctx := context.Background()
+
+				pausePipeline, _, err := intClient.PipelineConfigs.Create(ctx, mockTestingGroup, &Pipeline{
+					Name: pipelineName,
+				})
+
+				pp, _, err := intClient.Pipelines.Pause(context.Background(), pausePipeline.Name)
+				assert.NoError(t, err)
+				assert.True(t, pp)
+
+				deleteResponse, _, err := intClient.PipelineConfigs.Delete(ctx, pausePipeline.Name)
+				assert.Equal(t, "", deleteResponse)
 			}
-			fmt.Fprint(w, "")
 		})
-		pp, _, err := client.Pipelines.Pause(context.Background(), fmt.Sprintf("test-pipeline%d", n))
-		if err != nil {
-			assert.Nil(t, err)
-		}
-		assert.True(t, pp)
 	}
 }
 
